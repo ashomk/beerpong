75ddbc1f07455f9747d22953e2eb3302302a5ec2
Ashok Mathew Kuruvilla <akuruvil@usc.edu>
2016-04-12 18:57:36 -0700

    Adding audio : first cut

---
+++ Assets/Scripts/GamePlay/CollisionAudioPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionAudioPlayer : MonoBehaviour {
+
+	[Range (0, 1)]
+	public float volumeFactor = 1f;
+
+	void OnTriggerEnter(Collider other) {
+
+		OnCollisionEnter (null);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		
+		AudioSource audio = GetComponent<AudioSource> ();
+
+		if (audio.isPlaying) {
+			
+			audio.Stop ();
+		}
+
+		float velocity = 5f;
+		if (col != null) {
+
+			velocity = col.relativeVelocity.magnitude;
+		}
+
+		if (velocity > 0) {
+			
+			audio.volume = Mathf.Clamp01 (volumeFactor * velocity / 5f);
+			audio.Play ();
+		}
+	}
+}
