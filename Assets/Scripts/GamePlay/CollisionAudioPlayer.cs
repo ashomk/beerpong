964567963a54039abcbccb6e14ee697c3c04386a
Ashok Mathew Kuruvilla <akuruvil@usc.edu>
2016-04-13 19:07:51 -0700

    Updated audio and tiki cups

--- Assets/Scripts/GamePlay/CollisionAudioPlayer.cs
+++ Assets/Scripts/GamePlay/CollisionAudioPlayer.cs
@@ -3,16 +3,48 @@ using System.Collections;
 
 public class CollisionAudioPlayer : MonoBehaviour {
 
+	private BeerPong _beerPongGame;
+	private BeerPong beerPongGame {
+
+		get {
+
+			if (_beerPongGame == null) {
+
+				_beerPongGame = GetComponentInParent<BeerPong> ();
+			}
+
+			return _beerPongGame;
+		}
+	}
+
 	[Range (0, 1)]
 	public float volumeFactor = 1f;
 
 	void OnTriggerEnter(Collider other) {
 
-		OnCollisionEnter (null);
+		if (beerPongGame == null || transform.position.y < beerPongGame.transform.position.y) {
+			
+			return;
+		}
+		
+		if (other.GetComponent<Ball> () != null) {
+
+			OnCollisionEnter (null);
+		}
 	}
 
 	void OnCollisionEnter(Collision col) {
+
+		if (beerPongGame == null || transform.position.y < beerPongGame.transform.position.y) {
+			
+			return;
+		}
 		
+		if (col != null && col.collider.GetComponent<Ball> () == null) {
+		
+			return;
+		}
+
 		AudioSource audio = GetComponent<AudioSource> ();
 
 		if (audio.isPlaying) {
